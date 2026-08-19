4b51486199f13585fee1133c3bd698372d5f2cbf
Jossy Solá <jossysola@gmail.com>
2026-08-19 03:50:08 -0600

    Adds extra properties to User class

--- Data/ApplicationUser.cs
+++ Data/ApplicationUser.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace neurosintergia.Data;
@@ -5,5 +6,9 @@ namespace neurosintergia.Data;
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
+    [Required(ErrorMessage = "El rol es requerido")]
+    public required string Role { get; set; }
+    public bool IsMFAEnabled { get; set; } = false;
+    public bool IsBlocked { get; set; } = false;
+    public DateTime LastFailedAttempt { get; set; }
 }
-
