acfa97ec334d09ba7705626bde6c46d7131bc7bf
Jossy Solá <jossysola@gmail.com>
2026-08-21 00:31:00 -0600

    Model update

--- Data/ApplicationUser.cs
+++ Data/ApplicationUser.cs
@@ -1,4 +1,3 @@
-using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace neurosintergia.Data;
@@ -6,9 +5,6 @@ namespace neurosintergia.Data;
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
-    [Required(ErrorMessage = "El rol es requerido")]
-    public required string Role { get; set; }
-    public bool IsMFAEnabled { get; set; } = false;
     public bool IsBlocked { get; set; } = false;
-    public DateTime LastFailedAttempt { get; set; }
+    public string? BlockReason { get; set; }
 }
