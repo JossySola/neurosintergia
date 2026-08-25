d16dd7553e2ad066d27b492071d69a714d2132c0
Jossy Solá <jossysola@gmail.com>
2026-08-25 17:34:18 -0600

    Encodes link argument

--- Services/EmailSender.cs
+++ Services/EmailSender.cs
@@ -1,19 +1,22 @@
 using Resend;
+using System.Text.Encodings.Web;
 
 namespace neurosintergia.Services;
 
 public class EmailSender(IResend resend)
 {
     private readonly IResend _resend = resend;
-    public async Task Execute()
+    public async Task<ResendResponse> ExecuteConfirmation(string email, string link)
     {
+        var encodedLink = HtmlEncoder.Default.Encode(link);
         var message = new EmailMessage
         {
-            From = "",
-            Subject = "",
-            HtmlBody = ""
+            From = "contact@jossysola.com",
+            Subject = "Neurosintergia: Confirma tu correo electrónico",
+            HtmlBody = $"<p>Confirma tu correo electrónico dando clic en este enlace: <a href=\"{encodedLink}\">confirmar correo electrónico</a>.</p>",
+            TextBody = $"Confirm your email by clicking this link: {link}"
         };
-        message.To.Add("");
-        await _resend.EmailSendAsync(message);
+        message.To.Add($"{email}");
+        return await _resend.EmailSendAsync(message);
     }
 }
